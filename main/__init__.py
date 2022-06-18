cd12a95c730495772e1fc796fc9bf719230a8deb
Kavindu Athaudha <kavindu@k-ih.co.uk>
2022-06-18 23:47:06 +1200

    UERP-4 Connect to the Azure Cosmos DB and setup the base model
    Improved a #type: ignore.

--- main/__init__.py
+++ main/__init__.py
@@ -7,4 +7,4 @@ nest_asyncio.apply()
 async def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
   """Each request is redirected to the ASGI handler.
     """
-  return func.AsgiMiddleware(app).handle(req, context)  # type: ignore
+  return func.AsgiMiddleware(app).handle(req, context)  # type: ignore[no-untyped-call]
